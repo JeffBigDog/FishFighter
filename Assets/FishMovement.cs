cb1b2c000effc63476f8f2e809af14d5756c691c
Sam <sam.meurice@gmail.com>
2024-12-28 17:42:30 -0500

    Make player fish sprite flip horizontally when changing direction

--- Assets/FishMovement.cs
+++ Assets/FishMovement.cs
@@ -4,6 +4,7 @@ public class FishMovement : MonoBehaviour
 {
     public float speed = 5f;
     private Vector2 moveDirection;
+    private bool facingRight = true;
 
     void Update()
     {
@@ -16,10 +17,32 @@ public class FishMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveDirection = new Vector2(moveX, moveY).normalized;
+
+        // Check if we need to flip the sprite based on horizontal input
+        if (moveX > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveX < 0 && facingRight)
+        {
+            Flip();
+        }
     }
 
     void Move()
     {
         transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        
+        // Invert the x-scale to flip the fish horizontally
+        transform.localScale = new Vector3(
+            -transform.localScale.x,
+            transform.localScale.y,
+            transform.localScale.z
+        );
     }
 }
